9e0a55e72666fa3fc158f26c450759776f329c08
xiaoyu <jiangzhong91@gmail.com>
2024-07-17 02:41:23 +0800

    update Jwt string to json logic

--- src/main/java/com/github/xiaoyu2017/bbs/util/JwtUtil.java
+++ src/main/java/com/github/xiaoyu2017/bbs/util/JwtUtil.java
@@ -1,7 +1,10 @@
 package com.github.xiaoyu2017.bbs.util;
 
 import com.github.xiaoyu2017.bbs.pojo.bean.Payload;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 import java.security.PrivateKey;
 import java.security.PublicKey;
@@ -29,12 +32,7 @@ public class JwtUtil {
      * @return JWT
      */
     public static String generateTokenExpireInMinutes(Object userInfo, PrivateKey privateKey, int expire) {
-        return Jwts.builder()
-                .claim(JWT_PAYLOAD_USER_KEY, JsonUtils.toString(userInfo))
-                .setId(createJTI())
-                .setExpiration(dateUtil(expire))
-                .signWith(SignatureAlgorithm.RS256, privateKey)
-                .compact();
+        return generateTokenExpireInSeconds(userInfo, privateKey, expire * 60);
     }
 
     /**
@@ -46,12 +44,21 @@ public class JwtUtil {
      * @return JWT
      */
     public static String generateTokenExpireInSeconds(Object userInfo, PrivateKey privateKey, int expire) {
-        return Jwts.builder()
-                .claim(JWT_PAYLOAD_USER_KEY, userInfo)
-                .setId(createJTI())
-                .setExpiration(dateUtil(expire))
-                .signWith(SignatureAlgorithm.RS256, privateKey)
-                .compact();
+        if (userInfo instanceof String) {
+            return Jwts.builder()
+                    .claim(JWT_PAYLOAD_USER_KEY, userInfo)
+                    .setId(createJTI())
+                    .setExpiration(dateUtil(expire))
+                    .signWith(SignatureAlgorithm.RS256, privateKey)
+                    .compact();
+        } else {
+            return Jwts.builder()
+                    .claim(JWT_PAYLOAD_USER_KEY, JsonUtils.toString(userInfo))
+                    .setId(createJTI())
+                    .setExpiration(dateUtil(expire))
+                    .signWith(SignatureAlgorithm.RS256, privateKey)
+                    .compact();
+        }
     }
 
     /**
@@ -81,7 +88,13 @@ public class JwtUtil {
         Claims body = claimsJws.getBody();
         Payload<T> claims = new Payload<>();
         claims.setId(body.getId());
-        claims.setUserInfo(JsonUtils.toBean(body.get(JWT_PAYLOAD_USER_KEY).toString(), userType));
+        String typeName = userType.getTypeName();
+        typeName = typeName.substring(typeName.lastIndexOf('.') + 1);
+        if ("String".equals(typeName)) {
+            claims.setUserInfo((T) body.get(JWT_PAYLOAD_USER_KEY).toString());
+        } else {
+            claims.setUserInfo(JsonUtils.toBean(body.get(JWT_PAYLOAD_USER_KEY).toString(), userType));
+        }
         claims.setExpiration(body.getExpiration());
         return claims;
     }
@@ -107,4 +120,12 @@ public class JwtUtil {
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
 
+    public static void main(String[] args) throws Exception {
+        String x = "sasasasasasasasas";
+        String string = generateTokenExpireInSeconds(x, RsaUtil.getPrivateKey("/Users/yujiangzhong/IdeaProjects/Java/id_rsa"), 30);
+
+        PublicKey publicKey = RsaUtil.getPublicKey("/Users/yujiangzhong/IdeaProjects/Java/id_rsa.pub");
+        Payload<String> infoFromToken = getInfoFromToken("eyJhbGciOiJSUzI1NiJ9.eyJVU0VSX0lORk8iOiI1NjFiODQ2YWNhNGRkZWFhNGU0ODYzYzg2OWM5MmU2ZSIsImp0aSI6IlltSmhORFU1TURrdFpXRmhNeTAwT0RObExXSTJaV010WmpobE4ySmpZakUwT1RrNSIsImV4cCI6MTcyMTQxMTk1Nn0.kbBM3Mf6REOyDEdyYkwe2WR9UqNYjeW9BIhvQ_N68Lkms3HGCfCCpqZ_msdw37o4PUKNYC0V8ROi28qU9cABLbxJxWgukPp1aVX6lUsrDCJoiak4fBpIXmctsByo0UdV8sJBRoaLaEwig8Y883ssHF3PS6bv7ZKmUH8fuJpGbds4DpUlvwco5F2dbCGLaZg0CSBHjIW8BCM9FpZz6Sr_Q0QWs3XVdPj0za1LEKdRGD87kQo5w__BMQusKFikFw4CMvMG0DcPDzsuKcF7n6rIRbE8ufVcslhc-caeIgzzvvoTYIdik-uiKn3xMZJuOTXc1v1wCJe4L7wcIiGCMB_Ivw", publicKey, String.class);
+        System.out.println(infoFromToken.getUserInfo());
+    }
 }
