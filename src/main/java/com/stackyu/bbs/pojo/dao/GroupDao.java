9c6700293861380b49e26228e20074e8a1d2b7ad
xiaoyu <jiangzhong91@gmail.com>
2024-07-20 15:17:45 +0800

    add role entity

---
+++ src/main/java/com/stackyu/bbs/pojo/dao/GroupDao.java
@@ -0,0 +1,17 @@
+package com.stackyu.bbs.pojo.dao;
+
+import com.stackyu.bbs.pojo.BaseEntity;
+
+import java.io.Serializable;
+
+/**
+ * 用户组-DAO
+ *
+ * @author xiaoyu
+ * @version 1.0
+ */
+public class GroupDao extends BaseEntity implements Serializable {
+    private static final long serialVersionUID = -2368004483597426727L;
+
+    private String groupName;
+}
---
+++ src/main/java/com/stackyu/bbs/pojo/dao/RoleDao.java
@@ -0,0 +1,18 @@
+package com.stackyu.bbs.pojo.dao;
+
+import com.stackyu.bbs.pojo.BaseEntity;
+
+import java.io.Serializable;
+
+/**
+ * 角色类-DAO
+ *
+ * @author xiaoyu
+ * @version 1.0
+ */
+public class RoleDao extends BaseEntity implements Serializable {
+    private static final long serialVersionUID = 5432137152884006287L;
+
+    private String roleName;
+    private String roleId;
+}
